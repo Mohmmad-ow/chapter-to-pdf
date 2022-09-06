70ab8a176f0f4860b78672d87d962fca58ade4c6
Mohmmad-ow <moemessi00020@gmail.com>
2022-09-06 10:46:44 +0300

    Ready for deployment

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const ejs = require('ejs')
 const path = require('path')
+const findRemoveSync = require('find-remove')
 
+// Port
+const port = process.env.PORT || 3000;
 const scrapeDataAndWriteToPdf = require('./get-data.js')
 
 // app initiations
@@ -17,21 +20,42 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.post('/chapter-link-check', (req, res) => {
-    const link = req.body.chapterLink;
-    scrapeDataAndWriteToPdf(link, res)
+// in case you go to this URL without a post request then it gives the user a 404 error and links him back the home page
+app.get('/chapter-link-check', (req, res) => {
+    res.render('file-not-found')
     
 })
+// Make the file and writes it and when it's done it sends you to another page to download the file
+app.post('/chapter-link-check', (req, res) => {
+    const link = req.body.chapterLink;
+    if (!link || link == '') {
+        res.render('file-not-found')
+    } else {
 
+        scrapeDataAndWriteToPdf(link, res)
+    }
+    
+})
+// Download the ready file
+app.get('/download', (req, res) => {
+    
+        res.render('file-not-found')
+    
+})
 app.post('/download', (req, res) => {
     const file = req.body.filePath
-    console.log(file)
-    res.download(file)
+    if (!file || file == "") {
+        res.render('file-not-found')
+    } else {
+        console.log(file)
+        res.download(file)
+    }
 })
 
-app.listen(3000, () => {
-    // setInterval(() => {
-    //     console.log('Testing')
-    // }, 2000)
+app.listen(port, () => {
+    
+    setInterval(() => {
+        findRemoveSync(path.join(__dirname , 'public', 'chapter-pdf'), {age: { seconds: 1200 },extensions: '.pdf'})
+    }, 1800000)
     console.log('server listing on port 3000')
 })
--- get-data.js
+++ get-data.js
@@ -1,14 +1,9 @@
 // 
 const cheerio = require('cheerio');
 const axios = require('axios');
-const pretty = require('pretty');
 const pdfDoc = require('pdfkit');
 const fs = require('fs');
 const path = require('path')
-
-let url ='https://noobchan.xyz/novel/kanojo-ga-senpai-ni-ntr-reta-no-de/chapter-50/';
-
-
 module.exports = 
 
 async function (url, res) {
@@ -19,14 +14,16 @@ async function (url, res) {
       const $ = cheerio.load(data, null, false,{ ignoreWhitespace: true });
       // Chapter Title
       const title = $('.main-col h1').text()
+      // make a verb for the filePath to download the pdf file when it's made
       const filePath = path.join(__dirname , "public" , "chapter-pdf" , title + '.pdf')
       // Chapter Content
       const text = $('.text-left p')
-
-      // console.log(pretty(text.html()))
+      // we make a pdf file and write the title with a bit of styling
       const pdfDocument = new pdfDoc;
       pdfDocument.pipe(fs.createWriteStream(path.join(__dirname , "public" , "chapter-pdf" , title + '.pdf')));
       pdfDocument.fontSize(32).text(title, {align: 'center'});
+      // we loop through the content of the chapter and write each line to the pdf
+      // Note that we made an if statement to remove the U+3000 character as it leaves 0 in it's place if it's printed in the pdf file
       text.each(function (idx, line) {
         
         let l = $(line).text();
@@ -42,12 +39,12 @@ async function (url, res) {
           pdfDocument.moveDown()
         }
       });
+      // we close the PDF file when it's done
       pdfDocument.end()
+      // we render the next page and send the file path for the pdf file to be downloaded in the Download page
       res.render('download.ejs', {path: filePath})
     } catch (err) {
       console.log("There is an error " + err);
+      res.send("<h1>There was an error: make sure to enter a working link.</h1>")
     }
   }
-
-  // Invoke the above function
-  // console.log(scrapeDataAndWriteToPdf(url));
